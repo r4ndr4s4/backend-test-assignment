5a43a724d958c24cb47521ec209f0dcf63cc1fd6
Andras Rakita <andras.rakita@pentech.hu>
2023-02-16 14:24:01 +0100

    feat: init getUserDetails endpoint

--- src/controllers/users/getUserDetails.ts
+++ src/controllers/users/getUserDetails.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express";
 
-const getUserDetails = async (_: Request, res: Response): Promise<Response> => {
+import sql from "../../database";
+
+const getUserDetails = async (
+  { params }: Request,
+  res: Response
+): Promise<Response> => {
+  const { userId } = params;
+
+  console.log({ userId });
+
+  const user = await sql`
+    select name, email from users where id=${userId}
+  `;
+
   return res.json({
-    status: "getUserDetails OK",
+    data: user,
   });
 };
 
