8dabc8bb0bb612e32a42af3befa1882dd1e35375
Andras Rakita <andras.rakita@pentech.hu>
2023-02-16 18:07:38 +0100

    feat: users can only update/delete their projects

--- src/controllers/projects/deleteProject.ts
+++ src/controllers/projects/deleteProject.ts
@@ -1,15 +1,26 @@
 import { Request, Response } from "express";
+import assert from "assert";
 
 import sql from "../../database";
 import { Project } from "../../types";
 
 const deleteProject = async (
-  { params }: Request,
+  { params, auth }: Request,
   res: Response
 ): Promise<Response> => {
   const { projectId } = params;
 
   console.log({ projectId });
+
+  // TODO move to middleware?
+  const projectOwner = await sql<Project[]>`
+    select owner_id from projects where id=${projectId} and deleted=false
+  `;
+
+  assert(
+    projectOwner[0].owner_id === auth.userId,
+    "Users can only delete their own projects."
+  );
 
   const project = await sql<Project[]>`
     update projects set deleted=true where id=${projectId}
--- src/controllers/projects/updateProject.ts
+++ src/controllers/projects/updateProject.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from "express";
+import assert from "assert";
 
 import sql from "../../database";
 import { Project } from "../../types";
 
 const updateProject = async (
-  { params, body }: Request,
+  { params, body, auth }: Request,
   res: Response
 ): Promise<Response> => {
   const { projectId } = params;
   const { name } = body;
 
   console.log({ projectId, name });
+
+  // TODO move to middleware?
+  const projectOwner = await sql<Project[]>`
+    select owner_id from projects where id=${projectId} and deleted=false
+  `;
+
+  assert(
+    projectOwner[0].owner_id === auth.userId,
+    "Users can only update their own projects."
+  );
 
   const project = await sql<Project[]>`
     update projects set name=${name} where id=${projectId} and deleted=false
