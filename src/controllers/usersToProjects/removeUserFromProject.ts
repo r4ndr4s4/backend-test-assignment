c6f22657204778434a2a34dd60c13aa04bb35c0b
Andras Rakita <andras.rakita@pentech.hu>
2023-02-16 20:40:05 +0100

    feat: init removeUserFromProject endpoint

--- src/controllers/usersToProjects/removeUserFromProject.ts
+++ src/controllers/usersToProjects/removeUserFromProject.ts
@@ -1,11 +1,32 @@
 import { Request, Response } from "express";
+import assert from "assert";
+
+import sql from "../../database";
+import { Project, UserToProject } from "../../types";
 
 const removeUserFromProject = async (
-  _: Request,
+  { params, auth }: Request,
   res: Response
 ): Promise<Response> => {
+  const { projectId, userId } = params;
+
+  // TODO move to middleware?
+  const projectOwner = await sql<Project[]>`
+    select owner_id from projects where id=${projectId} and deleted=false
+  `;
+
+  assert(
+    projectOwner[0].owner_id === auth.userId || userId === auth.userId,
+    "Users can only delete themselves or get deleted by the owner of the project."
+  );
+
+  const userToProject = await sql<UserToProject[]>`
+    update users_to_projects set deleted=true where project_id=${projectId} and user_id=${userId}
+    returning *
+  `;
+
   return res.json({
-    status: "removeUserFromProject OK",
+    data: userToProject,
   });
 };
 
