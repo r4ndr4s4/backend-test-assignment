fc0686819b10a1a20037fa93122e4cbe8e45265f
Andras Rakita <andras.rakita@pentech.hu>
2023-02-16 15:39:05 +0100

    feat: users can only update/delete themselves

--- src/controllers/users/deleteUser.ts
+++ src/controllers/users/deleteUser.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
+import assert from "assert";
 
 import sql from "../../database";
 import { User } from "../../types";
 
 const deleteUser = async (
-  { params }: Request,
+  { params, auth }: Request,
   res: Response
 ): Promise<Response> => {
   const { userId } = params;
 
   console.log({ userId });
+
+  assert(userId === auth.userId, "Users can only delete themselves.");
 
   const user = await sql<User[]>`
     update users set deleted=true where id=${userId}
--- src/controllers/users/updateUser.ts
+++ src/controllers/users/updateUser.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from "express";
+import assert from "assert";
 
 import sql from "../../database";
 import { User } from "../../types";
 
 const updateUser = async (
-  { params, body }: Request,
+  { params, body, auth }: Request,
   res: Response
 ): Promise<Response> => {
   const { userId } = params;
   const { name, email } = body;
 
   console.log({ userId, name, email });
+
+  assert(userId === auth.userId, "Users can only update themselves.");
 
   const user = await sql<User[]>`
     update users set name=${name}, email=${email} where id=${userId}
