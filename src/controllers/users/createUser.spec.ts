f4ec4b6fc255cd46b0170f142fe6ece2936160fb
Andras Rakita <andras.rakita@pentech.hu>
2023-02-18 18:59:27 +0100

    feat: finish createUser tests

--- src/controllers/users/createUser.spec.ts
+++ src/controllers/users/createUser.spec.ts
@@ -3,6 +3,7 @@ import express from "express";
 
 import { asyncHandler } from "../../server";
 import auth from "../../middlewares/auth";
+import error from "../../middlewares/error";
 
 import createUser from "./createUser";
 import getUserDetails from "./getUserDetails";
@@ -20,6 +21,51 @@ describe("Users controller", () => {
   app.get("/users/:userId", asyncHandler(getUserDetails));
   app.patch("/users/:userId", asyncHandler(updateUser));
   app.delete("/users/:userId", asyncHandler(deleteUser));
+
+  app.use(error);
+
+  it("createUser should send 401 (missing auth header)", async () => {
+    const timestamp = Date.now();
+
+    await request(app)
+      .post("/users")
+      .send({
+        name: `Rakita András Teszt ${timestamp}`,
+        email: `Rakita.Andras+${timestamp}@gmail.com`,
+      })
+      .expect(401, {
+        status: "error",
+        message: "X-Auth-User-Email header not found.",
+      });
+  });
+
+  it("createUser should send 401 (wrong auth header)", async () => {
+    const timestamp = Date.now();
+
+    await request(app)
+      .post("/users")
+      .send({
+        name: `Rakita András Teszt ${timestamp}`,
+        email: `Rakita.Andras+${timestamp}@gmail.com`,
+      })
+      .set("X-Auth-User-Email", "xiao@lin.cn")
+      .expect(401, {
+        status: "error",
+        message: "Invalid X-Auth-User-Email header provided.",
+      });
+  });
+
+  it("createUser should send 500 (missing params)", async () => {
+    await request(app)
+      .post("/users")
+      .send({})
+      .set("X-Auth-User-Email", "admin@admin.com")
+      .expect(500, {
+        status: "error",
+        errorCode: "validation_error",
+        message: "email is a required field",
+      });
+  });
 
   it("createUser should send 200", async () => {
     const timestamp = Date.now();
